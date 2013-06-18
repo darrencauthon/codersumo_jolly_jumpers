df5ceb45f3371091104fd41f363076de316622fb
Darren Cauthon <darren@cauthon.com>
2013-06-17 22:56:00 -0500

    More working examples.

--- jj.rb
+++ jj.rb
@@ -1,5 +1,16 @@
 module JJ
   def self.is_jolly input
+    return false if input.count <= 1
+    expected_result = (1..input.count).to_a.reverse.to_a
+    expected_result.shift
+    things = input.each_with_index.select { |value, index| index > 0 }.
+      map do |value, index|
+        (value - input[index-1]).abs
+      end
+    things.reverse! if things.first < things.last
+    expected_result.each_with_index do |_, i|
+      return false if expected_result[i] != things[i]
+    end
     true
   end
 end
--- jj_spec.rb
+++ jj_spec.rb
@@ -4,9 +4,28 @@ require 'minitest/pride'
 require File.expand_path(File.dirname(__FILE__) + '/jj')
 
 describe JJ do
-  describe "a good jolly jumper" do
-    it "should return true" do
-      JJ.is_jolly([4, 3, 2, 1]).must_equal true
+  [
+    [1, 4, 2, 3],
+    [1, 2],
+    [12, 13, 15, 18]
+  ].each do |example|
+    describe "a good jolly jumper" do
+      it "should return true" do
+        JJ.is_jolly(example).must_equal true
+      end
+    end
+  end
+
+  [
+    [],
+    [1],
+    [1, 4, 2, -1, 6],
+    [33, 33, 33, 33]
+  ].each do |example|
+    describe "a good jolly jumper" do
+      it "should return true" do
+        JJ.is_jolly(example).must_equal false
+      end
     end
   end
 end
