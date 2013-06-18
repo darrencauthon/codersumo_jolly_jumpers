fbb7625b4723cedc43bf1b6f0777a5ef329f8e0b
Darren Cauthon <darren@cauthon.com>
2013-06-17 23:03:04 -0500

    Refactor.

--- jj.rb
+++ jj.rb
@@ -1,16 +1,28 @@
 module JJ
-  def self.is_jolly input
-    return false if input.count <= 1
-    expected_result = (1..input.count).to_a.reverse.to_a
-    expected_result.shift
-    things = input.each_with_index.select { |value, index| index > 0 }.
-      map do |value, index|
-        (value - input[index-1]).abs
-      end
-    things.reverse! if things.first < things.last
-    expected_result.each_with_index do |_, i|
-      return false if expected_result[i] != things[i]
+  def self.is_jolly numbers
+    expected_results = the_expected_result_for numbers
+    differences = differences_between numbers
+    differences.reverse! if differences.first < differences.last
+    expected_results.each_with_index do |v, i|
+      return false if v != differences[i]
     end
     true
+  rescue
+    false
+  end
+
+  private
+
+  def self.differences_between numbers
+    numbers.each_with_index.select { |_, index| index > 0 }.
+      map do |value, index|
+        (value - numbers[index-1]).abs
+      end
+  end
+
+  def self.the_expected_result_for numbers
+    expected_result = (1..numbers.count).to_a.reverse
+    expected_result.shift
+    expected_result
   end
 end
